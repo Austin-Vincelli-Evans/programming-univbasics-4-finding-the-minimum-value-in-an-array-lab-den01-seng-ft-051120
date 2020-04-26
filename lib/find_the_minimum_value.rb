a4f2c3414e65118db87e0b6b857ae39c1a660b62
Austin Evans <unity7591@gmail.com>
2020-04-26 19:43:43 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,4 +1,11 @@
 def find_min_value(array)
   counter = 0 
-  minimum =
+  minimum = 999
+  while count < array.length do
+   if minimum > array[count]
+     minimum = array[count]
+   end
+   count += 1 
+  end
+  minimum
 end
